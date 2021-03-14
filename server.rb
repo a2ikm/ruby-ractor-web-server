fea9ef184954dc791314e62b262dd896f36f04f1
Masato Ikeda <masato.ikeda@gmail.com>
2021-03-14 18:47:23 +0900

    Monitor crashes of workers and listener

--- server.rb
+++ server.rb
@@ -2,13 +2,7 @@ require "socket"
 
 CORES = %x{sysctl -n hw.ncpu}.chomp.to_i
 
-pipe = Ractor.new do
-  loop do
-    Ractor.yield(Ractor.receive, move:true)
-  end
-end
-
-workers = CORES.times.map do
+def create_worker(pipe)
   Ractor.new(pipe) do |pipe|
     loop do
       sock = pipe.take
@@ -22,12 +16,47 @@ workers = CORES.times.map do
       sock.print "Hello world! The time is #{Time.now}"
 
       sock.close
+    rescue => e
+      sock.close if sock && !sock.closed?
+      raise
     end
   end
 end
 
-server = TCPServer.new(8080)
+def create_listener(pipe)
+  Ractor.new(pipe) do |pipe|
+    server = TCPServer.new(8080)
+    loop do
+      sock = server.accept
+      pipe.send(sock, move: true)
+    end
+  rescue => e
+    server.shutdown
+    raise
+  end
+end
+
+pipe = Ractor.new do
+  loop do
+    Ractor.yield(Ractor.receive, move:true)
+  end
+end
+
+workers = CORES.times.map { create_worker(pipe) }
+listener = create_listener(pipe)
+
 loop do
-  sock = server.accept
-  pipe.send(sock, move: true)
+  Ractor.select(listener, *workers)
+rescue Ractor::RemoteError => e
+  if workers.include?(e.ractor)
+    $stderr.puts "recreating worker"
+    workers.delete(e.ractor)
+    workers << create_worker(pipe)
+  elsif listener == e.ractor
+    $stderr.puts "recreating listener"
+    listener = create_listerner(pipe)
+  else
+    raise "unknown ractor dead"
+  end
+  retry
 end
