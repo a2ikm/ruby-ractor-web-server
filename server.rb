a2bec9e60adb6ca3501c7e7e315df4038b9a9474
Masato Ikeda <masato.ikeda@gmail.com>
2021-03-14 15:01:44 +0900

    Add Ractor workers

--- server.rb
+++ server.rb
@@ -1,14 +1,27 @@
 require "socket"
 server = TCPServer.new(8080)
 
-while sock = server.accept
-  request = sock.gets
-  puts request
+CORES = %x{sysctl -n hw.ncpu}.chomp.to_i
 
-  sock.print "HTTP/1.1 200\r\n"
-  sock.print "Content-Type: text/html\r\n"
-  sock.print "\r\n"
-  sock.print "Hello world! The time is #{Time.now}"
+workers = CORES.times.map do
+  Ractor.new do
+    loop do
+      sock = Ractor.recv
 
-  sock.close
+      request = sock.gets
+      puts request
+
+      sock.print "HTTP/1.1 200\r\n"
+      sock.print "Content-Type: text/html\r\n"
+      sock.print "\r\n"
+      sock.print "Hello world! The time is #{Time.now}"
+
+      sock.close
+    end
+  end
+end
+
+loop do
+  sock = server.accept
+  workers.sample.send(sock, move: true)
 end
